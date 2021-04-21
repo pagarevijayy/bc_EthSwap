cc408a1116443012694ef6ff146a3c7fc051cbe3
pagarevijayy <pagarevj97@gmail.com>
2021-04-22 00:51:49 +0530

    project implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
+
+import EthSwap from '../abis/EthSwap.json';
+import Token from '../abis/Token.json';
+
 import Navbar from './Navbar'
+import Main from './Main'
+
 import './App.css';
 
 class App extends Component {
@@ -8,7 +14,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      account: ''
+      account: '',
+      ethBalance: '0',
+      tokenBalance: '0',
+      token: {},
+      ethSwap: {},
+      loading: true
     };
   }
 
@@ -39,19 +50,73 @@ class App extends Component {
     let ethBalance = await web3.eth.getBalance(this.state.account);
     this.setState({ ethBalance });
 
-    console.log(this.state);
+    const networkId = await web3.eth.net.getId();
 
+    // load token contract
+    const tokenData = Token.networks[networkId];
+
+    if (tokenData) {
+      const token = new web3.eth.Contract(Token.abi, tokenData.address);
+      this.setState({ token });
+
+      let tokenBalance = await token.methods.balanceOf(this.state.account).call();
+      this.setState({ tokenBalance: tokenBalance.toString() });
+
+    } else {
+      window.alert('Token contract not deployed to the detected network')
+    }
+
+    // load EthSwap contract
+    const ethSwapData = EthSwap.networks[networkId];
+
+    if (ethSwapData) {
+      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address);
+      this.setState({ ethSwap });
+
+    } else {
+      window.alert('EthSwap contract not deployed to the detected network')
+    }
+
+    this.setState({ loading: false })
+  }
+
+  buyTokens = (etherAmount) => {
+    this.setState({ loading: true });
+    this.state.ethSwap.methods.buyTokens().send({ value: etherAmount, from: this.state.account }).on('transactionHash', (hash) => {
+      this.setState({ loading: false });
+    });
+  }
+
+  sellTokens = (tokenAmount) => {
+    this.setState({ loading: true });
+    this.state.token.methods.approve(this.state.ethSwap.address, tokenAmount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+      this.state.ethSwap.methods.sellTokens(tokenAmount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false });
+      })
+    })
   }
 
   render() {
+    let content;
+    if (this.state.loading) {
+      content = <p id="loader" className="text-center p-5">Loading...</p>;
+    } else {
+      content = <Main
+        ethBalance={this.state.ethBalance}
+        tokenBalance={this.state.tokenBalance}
+        buyTokens={this.buyTokens}
+        sellTokens={this.sellTokens}
+      />;
+    }
+
     return (
       <div>
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
               <div className="content mr-auto ml-auto">
-                <h1 className="p-2"> Hello, world! </h1>
+                {content}
               </div>
             </main>
           </div>
